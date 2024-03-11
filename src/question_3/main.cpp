5eb2847f196e6b5e3480ea54bf1f519bdd9794a0
Tbritom2 <157549293+Tbritom2@users.noreply.github.com>
2024-03-11 02:40:32 +0000

    Question3

--- src/question_3/main.cpp
+++ src/question_3/main.cpp
@@ -1,6 +1,24 @@
+#include "question3.h"
+#include <iostream>
+
+using namespace std; 
 
 int main()
 {
+    int n;
+
+    cout << "Enter a number (1-15) to find its Fibonacci number: ";
+    cin >> n;
+ 
+    if (n < 1 || n > 15) {
+        cout << "Invalid input. Please enter a number between 1 and 15." << endl;
+        return 1; // Return error code
+    }
+
+
+    int fib_number = get_fib_sequence(n);
+
+    cout << "Fibonacci number for " << n << ": " << fib_number << endl;
 
     return 0;
 }
--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,22 @@
 #include "question3.h"
 
-bool test_config()
-{
-    return true;
+int get_fib_sequence(int n) {
+    if (n <= 0 || n > 15) {
+        return -1; 
+    }
+
+    int a = 0, b = 1, c;
+    if (n == 1) {
+        return a;
+    } else if (n == 2) {
+        return b;
+    }
+
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
 }
